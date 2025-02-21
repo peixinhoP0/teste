0bd7773c3a0128dc3050f6dbe5b6765c2ca98749
Victor Reis <95310368+peixinhoP0@users.noreply.github.com>
2025-02-21 08:44:46 -0300

    aula2.java

--- aula2.java
+++ aula2.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class ParOuImpar {
     public static void main(String[] args) {
-        // Criando um objeto Scanner para ler a entrada do usuário
+        // Criando um objeto entrada do usuário
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitando um número ao usuário
+        // Solicita um número ao usuário
         System.out.print("Digite um número: ");
         int numero = scanner.nextInt();
 
-        // Verificando se o número é par ou ímpar
+        // Verifica se o número é par ou ímpar
         if (numero % 2 == 0) {
             System.out.println("O número " + numero + " é par.");
         } else {
             System.out.println("O número " + numero + " é ímpar.");
         }
 
-        // Fechando o Scanner
+        
         scanner.close();
     }
 }
